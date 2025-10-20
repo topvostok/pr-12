4ad43236753878f59d33ce8a9c37d9571f655dd2
Григорий пеш <TOP_VOSTOK@outlook.com>
2025-10-20 22:12:42 +0500

    Добавьте файлы проекта.

---
+++ Book_Пешин/Classes/Genre.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Book_Пешин.Classes
+{
+    partial class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Genre(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public static List<Genre> AllGenres()
+        {
+            List<Genre> allGenres = new List<Genre>();
+            //Заполнение списка 
+            allGenres.Add(new Genre(1, "Современная Русская Литература"));
+            allGenres.Add(new Genre(2, "Современные Детективы"));
+            allGenres.Add(new Genre(3, "Любовное фэнтази"));
+            allGenres.Add(new Genre(4, "Попаданцы"));
+            allGenres.Add(new Genre(5, "Юмористическое Фэнтази"));
+
+            return allGenres;
+        }
+    }
+}
